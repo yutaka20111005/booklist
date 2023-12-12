03b018f80969e67d6324bd6f2d83c4548b5e0c32
yutaka20111005 <araki716y@gmail.com>
2023-12-12 22:14:52 +0900

    2nd commit

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-st.write("Book list\n")
-st.write("by Streamlit")
-st.markdown("### modeling")
+st.write("Book list by Streamlit\n")
+st.markdown("### 書籍一覧")
 
 st.write(pd.DataFrame({
   '書籍名': ['日経Linux（リナックス） 2024年1月号','Kubernetes完全ガイド 第2版','フロントエンドの知識地図―― 一冊でHTML/CSS/JavaScriptの開発技術が学べる本','独習 Pythonバイオ情報解析?Jupyter、NumPy、pandas、Matplotlibを理解し、実装して学ぶシングルセル、RNA-Seqデータ解析 (実験医学別冊)'],
