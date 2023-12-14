920cb6285c0c4afb7adce062162b79c6c6e4ad5b
yutaka20111005 <araki716y@gmail.com>
2023-12-14 14:15:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+"""  独学書籍一覧
+　独学者に有用な書籍、雑誌を価格含めて記載。
+"""
 import streamlit as st
 import pandas as pd
 
@@ -8,7 +11,8 @@ st.write(pd.DataFrame({
   '書籍名': ['日経Linux（リナックス） 2024年1月号',
           'Kubernetes完全ガイド 第2版',
           'フロントエンドの知識地図―― 一冊でHTML/CSS/JavaScriptの開発技術が学べる本',
-          '独習 Pythonバイオ情報解析?Jupyter、NumPy、pandas、Matplotlibを理解し、実装して学ぶシングルセル、RNA-Seqデータ解析 (実験医学別冊)'],
+          '独習 Pythonバイオ情報解析?Jupyter、NumPy、pandas、Matplotlibを理解し、\
+          実装して学ぶシングルセル、RNA-Seqデータ解析 (実験医学別冊)'],
   '価格': [2000,4400,2860,6600],
   '内容': ["2024年1月号が最後となりました。Linux最新の話題をまとめた雑誌はこれだけだったので残念。 \
   バックナンバーをま とめたのを１冊買って、過去から一気に手を動かして動作させるのはおススメです。",
